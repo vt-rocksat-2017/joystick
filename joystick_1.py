0406bd5491e5ca47ad97c2122792a734ef3b5cbe
Kat Hogue <kbh96@vt.edu>
2017-05-30 09:59:30 -0400

    Modified joystick_1.py to include speed and directional messages. Copied muffin.jpeg over.

--- joystick_1.py
+++ joystick_1.py
@@ -41,19 +41,36 @@ while done==False:
 
 	axis1 = joy1.get_axis(0)
 	axis2 = joy1.get_axis(1)
-
-	print axis1
-	print axis2
+	axisSpeed = joy1.get_axis(2)
 
 	if (axis1 < -0.5) and (mufX > 10):
 		mufX-= 1
+		print ("LEFT")
 	if (axis1 > 0.5) and (mufX < 900):
 		mufX += 1
+		print ("RIGHT")
 	if (axis2 < -0.5) and (mufY > 10):
 		mufY -= 1
+		print ("UP")
 	if (axis2 > 0.5) and (mufY < 650):
 		mufY += 1
+		print ("DOWN")
 
-	clock.tick(30)
+	if (axisSpeed > .75):
+		clock.tick(1)
+	elif (axisSpeed > .5):
+		clock.tick(2)
+	elif (axisSpeed > .25):
+		clock.tick(5)
+	elif (axisSpeed > 0):
+		clock.tick(10)
+	elif (axisSpeed > -.25):
+		clock.tick(20)
+	elif(axisSpeed > -.5):
+		clock.tick(30)
+	elif(axisSpeed > -.75):
+		clock.tick(45)
+	else:
+		clock.tick(60)
 
 pygame.joystick.quit()

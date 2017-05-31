2dcf98dcc9f3484cdb8994c18ab54ecf7780c168
kbhogue <kbh96@vt.edu>
2017-05-31 15:22:11 -0400

    Update joystick_1.py

--- joystick_1.py
+++ joystick_1.py
@@ -11,14 +11,14 @@ WHITE = (255, 255,255)
 done = False
 mufY = 10
 mufX = 10
+speed = 0
+
 #init
 pygame.joystick.init()
 pygame.init()
-
 screen = pygame.display.set_mode((1024,768))
 muf = pygame.image.load('muffin.jpeg')
 clock = pygame.time.Clock()
-
 joy1 = pygame.joystick.Joystick(0)
 joy1.init()
 
@@ -39,52 +39,60 @@ while done==False:
 	axis2 = joy1.get_axis(1)
 	axisSpeed = joy1.get_axis(2)
 
+	#Speed of change
+	if (axisSpeed > .75):
+		clock.tick(1)
+		speed = 8
+	elif (axisSpeed > .5):
+		clock.tick(2)
+		speed = 16
+	elif (axisSpeed > .25):
+		clock.tick(5)
+		speed = 24
+	elif (axisSpeed > 0):
+		clock.tick(10)
+		speed = 32
+	elif (axisSpeed > -.25):
+		clock.tick(20)
+		speed = 40
+	elif(axisSpeed > -.5):
+		clock.tick(30)
+		speed = 48
+	elif(axisSpeed > -.75):
+		clock.tick(45)
+		speed = 56
+	else:
+		clock.tick(60)
+		speed = 64
+		
 	#Changing directions
 	if (axis1 < -0.5) and (axis2 < -0.5) and (mufX > 10) and (mufY > 10):
 		mufX -= 1
 		mufY -= 1
-		print ("UP LEFT")
+		print ("Up Left - 570500000000000000001420")
 	elif (axis1 < -0.5) and (axis2 > 0.5) and (mufX > 10) and (mufY < 900):
 		mufX -= 1
 		mufY += 1
-		print ("DOWN LEFT")
+		print ("DOWN LEFT - 570A00000000000000001420")
 	elif (axis1 > 0.5) and (axis2 < -0.5) and (mufX < 650) and (mufY > 10):
 		mufX += 1
 		mufY -= 1
-		print ("UP RIGHT")
+		print ("UP RIGHT - 570600000000000000001420")
 	elif (axis1 > 0.5) and (axis2 > 0.5) and (mufX < 650) and (mufY < 900):
 		mufX += 1
 		mufY += 1
-		print ("DOWN RIGHT")
+		print ("DOWN RIGHT - 570900000000000000001420")
 	elif (axis1 < -0.5) and (mufX > 10):
 		mufX-= 1
-		print ("LEFT - 5700000000 ")
+		print ("LEFT - 570100000000000000001420")
 	elif (axis1 > 0.5) and (mufX < 900):
 		mufX += 1
-		print ("RIGHT")
+		print ("RIGHT - 570200000000000000001420")
 	elif (axis2 < -0.5) and (mufY > 10):
 		mufY -= 1
-		print ("UP")
+		print ("UP - 570400000000000000001420")
 	elif (axis2 > 0.5) and (mufY < 650):
 		mufY += 1
-		print ("DOWN")
-
-	#Speed of change
-	if (axisSpeed > .75):
-		clock.tick(1)
-	elif (axisSpeed > .5):
-		clock.tick(2)
-	elif (axisSpeed > .25):
-		clock.tick(5)
-	elif (axisSpeed > 0):
-		clock.tick(10)
-	elif (axisSpeed > -.25):
-		clock.tick(20)
-	elif(axisSpeed > -.5):
-		clock.tick(30)
-	elif(axisSpeed > -.75):
-		clock.tick(45)
-	else:
-		clock.tick(60)
+		print ("DOWN - 570800000000000000001420")
 
 pygame.joystick.quit()

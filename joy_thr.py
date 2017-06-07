c2b10550d6e3cf2a5d5e984a0fdac8becf21936d
Kat Hogue <kbh96@vt.edu>
2017-06-07 13:59:22 -0400

    Updated joystick thread to have self. statements

--- joy_thr.py
+++ joy_thr.py
@@ -11,16 +11,16 @@ from pygame.locals import *
 class Joystick_Thread(threading.Thread):
 
     def __init__(self, threadID, name, counter):
-	pygame.joystick.init()
-	pygame.init()
-	clock = pygame.time.Clock()
-	joy1 = pygame.joystick.Joystick(0)
-	joy1.init()
+	self.pygame.joystick.init()
+	self.pygame.init()
+	self.clock = pygame.time.Clock()
+	self.joy1 = pygame.joystick.Joystick(0)
+	self.joy1.init()
 
     def run(self):
 	while (1):
-	    axis1 = joy1.get_axis(0)
-	    axis2 = joy1.get_axis(1)
-	    axisSpeed = joy1.get_axis(2)
+	    self.axis1 = joy1.get_axis(0)
+	    self.axis2 = joy1.get_axis(1)
+	    self.axisSpeed = joy1.get_axis(2)
 
 
